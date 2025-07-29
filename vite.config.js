0670055a14a4723b73e6b80415a315084e4f205b
Ragul-M007 <ragulmahalingam007@gmail.com>
2025-07-29 16:06:51 +0530

    css

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // or use your IP: '192.168.1.100'
-    port: 5173 // default port, or any you like
-  }
-});
+    allowedHosts: ['livestreaming.emeetify.com'], // ✅ Allow the blocked host
+  },
+})
